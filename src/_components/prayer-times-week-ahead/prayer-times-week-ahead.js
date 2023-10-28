623892665df37f0b8167aa51b21cb98c902126d3
Abdullah Arafat <arafat@asia.com>
2023-10-28 18:43:38 +0800

    Added week view

--- src/_components/prayer-times-week-ahead/prayer-times-week-ahead.js
+++ src/_components/prayer-times-week-ahead/prayer-times-week-ahead.js
@@ -18,6 +18,7 @@ class PrayerTimesWeekAhead extends Component {
 
   fetchPrayerTimes() {
     const city = "Perth";
+    const country = "Australia";
 
     // Get the current year, month, and day
     const currentDate = moment();
@@ -28,7 +29,7 @@ class PrayerTimesWeekAhead extends Component {
     // Fetch prayer times from the Aladhan API starting from the current date
     axios
       .get(
-        `http://api.aladhan.com/v1/calendarByCity?city=${city}&country=Australia&method=2&month=${currentMonth}&year=${currentYear}&day=${currentDay}`
+        `http://api.aladhan.com/v1/calendarByCity?city=${city}&country=${country}&method=2&month=${currentMonth}&year=${currentYear}&day=${currentDay}`
       )
       .then((response) => {
         const data = response.data.data.slice(0, 7); // Limit to 7 days
