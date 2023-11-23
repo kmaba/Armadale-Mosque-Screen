cc383fd77ab58a2e1e2f5ea51f8d1724f4c6a32a
Abdullah Arafat <arafat@asia.com>
2023-11-23 22:38:24 +0800

    Updated Google Slides and window full-screen event

--- src/GoogleSlides/GoogleSlides.js
+++ src/GoogleSlides/GoogleSlides.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './GoogleSlides.css';
+import JummahTimes from '../_components/jummah-times/jummah-times';
+import PrayerTimes from '../_components/prayer-times/prayer-times';
 import config from '../config.json';
 
 class GoogleSlides extends Component {
@@ -23,16 +25,50 @@ class GoogleSlides extends Component {
 
   render() {
     return (
-      <div className="GoogleSlides" style={{ width: '100vw', height: '100vh' }}>
-        <iframe
-          title="GoogleSlidesIframe"
-          src={this.state.url}
-          frameBorder="0"
-          border="0"
-          width="100%"
-          height="100%"
-          allowFullScreen="true"
-        />
+      <div>
+        <div
+          className="GoogleSlides"
+          style={{ position: 'relative', width: '100vw', height: '100vh' }}
+        >
+          <iframe
+            title="GoogleSlidesIframe"
+            src={this.state.url}
+            frameBorder="0"
+            border="0"
+            width="100%"
+            height="100%"
+            allowFullScreen="true"
+          />
+          <div
+            className="content-over-slides"
+            style={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%'
+            }}
+          >
+            <div className="row">
+              <div className="col-12 col-md-6">
+                <div className="row"></div>
+              </div>
+              <div className="col-12 col-md-6">
+                <div className="row">
+                  <PrayerTimes />
+                </div>
+                <div className="col-12 col-md-6">
+                  <div className="row">
+                    <PrayerTimes />
+                  </div>
+                  <div className="row">
+                    <JummahTimes />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
