b4d45a747ec761b4341689279e0076007e7006ff
Abdullah Arafat <62858251+kmaba@users.noreply.github.com>
2023-12-18 11:43:32 +0800

    Fixed Logo Sizing

--- src/_components/logo/logo.js
+++ src/_components/logo/logo.js
@@ -21,7 +21,7 @@ class Logo extends Component {
         <img
           src={this.state.logo}
           alt=""
-          style={{ width: '48%', height: 'auto' }}
+          style={{ width: '35%', height: 'auto' }}
         />
       </div>
     );
