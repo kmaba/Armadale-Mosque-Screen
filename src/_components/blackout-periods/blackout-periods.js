17b591a99ddfb27bba766af0ff34ed3276623974
Abdullah Arafat <62858251+kmaba@users.noreply.github.com>
2024-01-01 14:33:37 +0800

    Fixed Blackout Periods

--- src/_components/blackout-periods/blackout-periods.js
+++ src/_components/blackout-periods/blackout-periods.js
@@ -3,6 +3,7 @@ import PrayerData from '../prayer-data/prayer-data';
 import moment from 'moment/moment';
 import View6 from '../../Views/View 6/View6';
 import AppConfig from '../app-config/app-config';
+import axios from 'axios';
 
 class BlackoutPeriods extends Component {
   constructor(props) {
@@ -17,8 +18,64 @@ class BlackoutPeriods extends Component {
         maghrib: _appConfig.get('blackOutPeriod_maghrib'),
         isha: _appConfig.get('blackOutPeriod_isha'),
         jummah: _appConfig.get('blackOutPeriod_jummah')
-      }
+      },
+      prayerTimes: {},
+      jamaahTimes: {}
     };
+  }
+
+  fetchPrayerTimes() {
+    const city = 'Perth';
+    const country = 'Australia';
+
+    // Get the current year, month, and day
+    const currentDate = moment();
+    const currentYear = currentDate.year();
+    const currentMonth = currentDate.month() + 1;
+    const currentDay = currentDate.date();
+
+    // Fetch prayer times from the Aladhan API starting from the current date
+    axios
+      .get(
+        `https://api.aladhan.com/v1/calendarByCity?city=${city}&country=${country}&method=3&month=${currentMonth}&year=${currentYear}`
+      )
+      .then(response => {
+        // Find today's data in the response
+        const todayData = response.data.data.find(
+          dayData =>
+            moment(dayData.date.readable, 'DD MMM YYYY').date() === currentDay
+        );
+
+        let data = todayData.timings; // Get today's prayer times
+
+        // Remove "(AWST)" and convert time format
+        for (let key in data) {
+          data[key] = moment(data[key].replace(' (AWST)', ''), 'HH:mm').format(
+            'h:mm'
+          );
+        }
+
+        this.setState({ prayerTimes: data });
+
+        // Get jamaah times from prayer-data.js
+        const prayerData = new PrayerData();
+        const jamaahTimes = prayerData.getPrayerTimes(
+          currentDate.format('DD/MM/YYYY')
+        );
+
+        // Add 10 minutes to Maghrib prayer time
+        jamaahTimes['maghrib_jamaah'] = moment(
+          this.state.prayerTimes['Maghrib'],
+          'h:mm'
+        )
+          .add(10, 'minutes')
+          .format('h:mm');
+
+        this.setState({ jamaahTimes });
+      })
+      .catch(error => {
+        console.error('Error fetching prayer times:', error);
+      });
   }
 
   getPrayerTimes() {
@@ -26,11 +83,21 @@ class BlackoutPeriods extends Component {
     var _data = new PrayerData();
     var currentDay = _data.getPrayerTimes(date);
     var prayerTimes = {
-      fajr: `${currentDay['fajr_jamaah']} AM`,
-      zuhr: `${currentDay['zuhr_jamaah']} PM`,
-      asr: `${currentDay['asr_jamaah']} PM`,
-      maghrib: `${currentDay['maghrib_jamaah']} PM`,
-      isha: `${currentDay['isha_jamaah']} PM`
+      fajr: moment(`${currentDay['fajr_jamaah']} AM`, 'h:mm A')
+        .subtract(1, 'minutes')
+        .format('h:mm A'),
+      zuhr: moment(`${currentDay['zuhr_jamaah']} PM`, 'h:mm A')
+        .subtract(1, 'minutes')
+        .format('h:mm A'),
+      asr: moment(`${currentDay['asr_jamaah']} PM`, 'h:mm A')
+        .subtract(1, 'minutes')
+        .format('h:mm A'),
+      maghrib: moment(`${currentDay['maghrib_jamaah']} PM`, 'h:mm A')
+        .subtract(1, 'minutes')
+        .format('h:mm A'),
+      isha: moment(`${currentDay['isha_jamaah']} PM`, 'h:mm A')
+        .subtract(1, 'minutes')
+        .format('h:mm A')
     };
     return prayerTimes;
   }
